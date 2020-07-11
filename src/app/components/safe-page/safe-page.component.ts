162124bd5c7a3f2fa4908955808d1c0f59a791bd
Mozifei <1927870057@qq.com>
2020-07-12 02:53:31 +0800

    完善安全中心首页

--- src/app/components/safe-page/safe-page.component.ts
+++ src/app/components/safe-page/safe-page.component.ts
@@ -9,7 +9,31 @@ export class SafePageComponent implements OnInit {
 
   constructor() { }
 
+  isMainPage = true;
+
+  isPasswordPage = false;
+
+  isEmailPage = false;
+
   ngOnInit(): void {
   }
 
+
+  jumpToChangePasswordPage(): void{
+    this.isMainPage = false;
+    this.isPasswordPage = true;
+    this.isEmailPage = false;
+  }
+
+  jumpToChangeEmailPage(): void{
+    this.isMainPage = false;
+    this.isEmailPage = true;
+    this.isPasswordPage = false;
+  }
+
+  backToMainPage(): void{
+    this.isMainPage = true;
+    this.isEmailPage = false;
+    this.isPasswordPage = false;
+  }
 }
--- src/app/components/user-info-page/user-info-page.component.ts
+++ src/app/components/user-info-page/user-info-page.component.ts
@@ -23,4 +23,5 @@ export class UserInfoPageComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
 }
